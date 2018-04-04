ef6b38c6bc38eb01501423d89b7eaa2735e5bddc
ifedun <ifedun@cogniance.com>
2018-04-04 12:52:17 +0300

    Added extent report that work in surfire plugin

---
+++ src/test/java/utils/ExtentTestNGIReporterListener.java
@@ -0,0 +1,83 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.testng.*;
+import org.testng.xml.XmlSuite;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+
+public class ExtentTestNGIReporterListener implements IReporter {
+
+    private ExtentReports report;
+    private ExtentHtmlReporter htmlReporter;
+
+    @Override
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+        init();
+
+        for (ISuite suite : suites) {
+
+            Map<String, ISuiteResult> result = suite.getResults();
+
+            for (ISuiteResult r : result.values()) {
+                ITestContext context = r.getTestContext();
+
+                buildTestNodes(context.getFailedTests(), Status.FAIL);
+                buildTestNodes(context.getSkippedTests(), Status.SKIP);
+                buildTestNodes(context.getPassedTests(), Status.PASS);
+
+            }
+        }
+
+        for (String s : Reporter.getOutput()) {
+            report.setTestRunnerOutput(s);
+        }
+
+        report.flush();
+    }
+
+    private void init() {
+        htmlReporter = new ExtentHtmlReporter(new File(System.getProperty("user.dir") + "/AutomationReport.html"));
+        htmlReporter.loadXMLConfig(new File(System.getProperty("user.dir") + "/extent-config.xml"));
+
+        report = new ExtentReports();
+        report.attachReporter(htmlReporter);
+        report.setReportUsesManualConfiguration(true);
+    }
+
+    private void buildTestNodes(IResultMap tests, Status status) {
+        ExtentTest test;
+
+        if (tests.size() > 0) {
+            for (ITestResult result : tests.getAllResults()) {
+                test = report.createTest(result.getMethod().getMethodName());
+
+                for (String group : result.getMethod().getGroups())
+                    test.assignCategory(group);
+
+                if (result.getThrowable() != null) {
+                    test.log(status, result.getThrowable());
+                } else {
+                    test.log(status, "Test " + status.toString().toLowerCase() + "ed");
+                }
+
+                test.getModel().setStartTime(getTime(result.getStartMillis()));
+                test.getModel().setEndTime(getTime(result.getEndMillis()));
+            }
+        }
+    }
+
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+}
